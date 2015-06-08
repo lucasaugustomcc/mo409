7905ed7ddfdf64266d9869679e7030720cec6431
Lucas Augusto <lucasaugustomcc@gmail.com>
2015-06-08 18:24:38 -0300

    Alterações no handleEvent

--- test/src/br/unicamp/ic/mo409/model/Chamada.java
+++ test/src/br/unicamp/ic/mo409/model/Chamada.java
@@ -14,7 +14,7 @@ public class Chamada {
 	public Chamada()
 	{
 		state = ChamadaState.nao_aberta;
-		setRecebendo_tick(false);
+		setRecebendoTick(false);
 	}
 
 	public void abrirChamada(ChamadaState state) {
@@ -30,64 +30,63 @@ public class Chamada {
 	}
 
 	public void handleEvent(Object... in_colObject) {
-		if (in_colObject.length > 0) {
-//			String sEventName = (String) in_colObject[0];
-//			if ((state == State.idle)
-//					&& (sEventName.compareTo("detectItemEvent") == 0)) {
-//				Item oItem = (Item) in_colObject[1];
-//				m_width = (oItem.width + 2) * 2;
-//				m_height = (oItem.height * 2) + 2;
-//				if (actualHeightIsLowerThanMinCompare()) {
-//					recognized = true;
-//				} else {
-//					recognized = false;
-//				}
-//				if (recognizedIsTrue()) {
-//					state = State.object_recognized;
-//					if (widthIsLessThanMinCompare()) {
-//						state = State.object_is_too_small;
-//					} else if (widthIsGreaterThanMaxCompare()) {
-//						state = State.object_is_too_big;
-//					} else if (widthIsBetweenMinCompareAndMaxCompare()) {
-//						state = State.object_is_fitting;
-//					}
-//				} else {
-//					state = State.object_not_recognized;
-//				}
-//			}
-//			if (sEventName.compareTo("resetEvent") == 0) {
-//				// object not recognized
-//				if (state == State.object_not_recognized
-//						|| state == State.object_is_fitting
-//						|| state == State.object_is_too_big
-//						|| state == State.object_is_too_small) {
-//					state = State.idle;
-//				}
-//			}
+		if (in_colObject.length > 0) 
+		{
+			String sEventName = (String) in_colObject[0];
+			if ((state == ChamadaState.nao_aberta)
+					&& (sEventName.compareTo("abrirChamadaEvent") == 0)) 
+			{
+					state = ChamadaState.aberta;
+			}
+			
+			if (sEventName.compareTo("receberTickEvent") == 0) 
+			{
+				if (state == ChamadaState.aberta) 
+				{
+					receberTick(state);
+					state = ChamadaState.aberta;
+				}
+			}
+			
+			if (sEventName.compareTo("encerrarChamadaEvent") == 0) 
+			{
+				if (state == ChamadaState.aberta) 
+				{
+					state = ChamadaState.encerrada;
+				}
+			}
+			
+			if (sEventName.compareTo("calcularPresencaEvent") == 0) 
+			{
+				if (state == ChamadaState.encerrada) 
+				{
+					state = ChamadaState.encerrada;
+				}
+			}
 		}
 	}
 
-	public void setRecebendo_tick(Boolean recebendo_tick) {
+	public void setRecebendoTick(Boolean recebendo_tick) {
 		this.recebendo_tick = recebendo_tick;
 	}
 
-	public Boolean getRecebendo_tick() {
+	public Boolean getRecebendoTick() {
 		return recebendo_tick;
 	}
 
-	public void setHora_inicio(Date hora_inicio) {
+	public void setHoraInicio(Date hora_inicio) {
 		this.hora_inicio = hora_inicio;
 	}
 
-	public Date getHora_inicio() {
+	public Date getHoraInicio() {
 		return hora_inicio;
 	}
 
-	public void setHora_fim(Date hora_fim) {
+	public void setHoraFim(Date hora_fim) {
 		this.hora_fim = hora_fim;
 	}
 
-	public Date getHora_fim() {
+	public Date getHoraFim() {
 		return hora_fim;
 	}
 }
