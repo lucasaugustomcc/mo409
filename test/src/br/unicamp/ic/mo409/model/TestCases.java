90fb31f779fa7adb6a4d15543ab59aeb211359e3
Lucas Augusto <lucasaugustomcc@gmail.com>
2015-06-08 17:37:29 -0300

    Adição do gitignore. Alteração do TestCases.

--- test/src/br/unicamp/ic/mo409/model/TestCases.java
+++ test/src/br/unicamp/ic/mo409/model/TestCases.java
@@ -6,6 +6,43 @@ import static org.junit.Assert.*;
 // JUnit 4.3
 public class TestCases{
 	
+	public static junit.framework.Test suite(){
+		return new junit.framework.JUnit4TestAdapter(TestCases.class);
+	}
 	
+	@Test
+	public void test1()
+	{
+		Chamada oTestObject = new Chamada();
+		ChamadaState state1 = ChamadaState.nao_aberta;
+		ChamadaState state3 = ChamadaState.nao_aberta;
+		ChamadaState state5 = ChamadaState.nao_aberta;
+		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
+		oTestObject.handleEvent("abrirChamadaEvent", state1);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		oTestObject.handleEvent("encerrarChamadaEvent", state3);
+		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
+		oTestObject.handleEvent("encerrarChamadaEvent", state5);
+		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
+		
+	}
+	
+	@Test
+	public void test2()
+	{
+		Chamada oTestObject = new Chamada();
+		ChamadaState state1 = ChamadaState.nao_aberta;
+		ChamadaState state3 = ChamadaState.nao_aberta;
+		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
+		oTestObject.handleEvent("abrirChamadaEvent", state1);
+		assertEquals(true, (state1 == ChamadaState.nao_aberta));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() == true));
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() == true));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() != false));
+		oTestObject.handleEvent("receberTickEvent", state3);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		
+	}
 	
 }
