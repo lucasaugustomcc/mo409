2dd1097c3940c89a8d9c8757fd94baca23ad719b
jevianna <jevianna@gmail.com>
2015-06-12 19:02:10 -0300

    ultimas alteracoes feitas na aula 11/jun

---
+++ test/src/br/unicamp/ic/mo409/model/ListaDeDisciplinas.java
@@ -0,0 +1,7 @@
+package br.unicamp.ic.mo409.model;
+
+public enum ListaDeDisciplinas {
+	MO409,
+	MO410,
+	MO210
+}
--- test/src/br/unicamp/ic/mo409/model/TestCases.java
+++ test/src/br/unicamp/ic/mo409/model/TestCases.java
@@ -14,15 +14,15 @@ public class TestCases{
 	public void test1()
 	{
 		Chamada oTestObject = new Chamada();
-		Integer disciplina1 = -268;
+		ListaDeDisciplinas disciplina1 = ListaDeDisciplinas.MO409;
 		ChamadaState state3 = ChamadaState.nao_aberta;
-		ChamadaState state5 = ChamadaState.nao_aberta;
+		Integer tick5 = -67;
 		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
 		oTestObject.handleEvent("abrirChamadaEvent", disciplina1);
 		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
 		oTestObject.handleEvent("encerrarChamadaEvent", state3);
 		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
-		oTestObject.handleEvent("calcularPresencaEvent", state5);
+		oTestObject.handleEvent("calcularPresencaEvent", tick5);
 		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
 		
 	}
@@ -31,8 +31,74 @@ public class TestCases{
 	public void test2()
 	{
 		Chamada oTestObject = new Chamada();
-		Integer disciplina1 = -343;
-		Integer tick3 = -5;
+		ListaDeDisciplinas disciplina1 = ListaDeDisciplinas.MO409;
+		ChamadaState state3 = ChamadaState.nao_aberta;
+		Integer tick5 = -176;
+		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
+		oTestObject.handleEvent("abrirChamadaEvent", disciplina1);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		oTestObject.handleEvent("encerrarChamadaEvent", state3);
+		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
+		oTestObject.handleEvent("calcularPresencaEvent", tick5);
+		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
+		
+	}
+	
+	@Test
+	public void test3()
+	{
+		Chamada oTestObject = new Chamada();
+		ListaDeDisciplinas disciplina1 = ListaDeDisciplinas.MO409;
+		ChamadaState state3 = ChamadaState.nao_aberta;
+		Integer tick5 = 304;
+		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
+		oTestObject.handleEvent("abrirChamadaEvent", disciplina1);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		oTestObject.handleEvent("encerrarChamadaEvent", state3);
+		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
+		oTestObject.handleEvent("calcularPresencaEvent", tick5);
+		assertEquals(true, (oTestObject.state == ChamadaState.encerrada));
+		
+	}
+	
+	@Test
+	public void test4()
+	{
+		Chamada oTestObject = new Chamada();
+		ListaDeDisciplinas disciplina1 = ListaDeDisciplinas.MO409;
+		Integer tick3 = -385;
+		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
+		oTestObject.handleEvent("abrirChamadaEvent", disciplina1);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() == true));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() != false));
+		oTestObject.handleEvent("receberTickEvent", tick3);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		
+	}
+	
+	@Test
+	public void test5()
+	{
+		Chamada oTestObject = new Chamada();
+		ListaDeDisciplinas disciplina1 = ListaDeDisciplinas.MO409;
+		Integer tick3 = -2;
+		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
+		oTestObject.handleEvent("abrirChamadaEvent", disciplina1);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() == true));
+		assertEquals(true, (oTestObject.recebendo_tick.booleanValue() != false));
+		oTestObject.handleEvent("receberTickEvent", tick3);
+		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
+		
+	}
+	
+	@Test
+	public void test6()
+	{
+		Chamada oTestObject = new Chamada();
+		ListaDeDisciplinas disciplina1 = ListaDeDisciplinas.MO409;
+		Integer tick3 = -217;
 		assertEquals(true, (oTestObject.state == ChamadaState.nao_aberta));
 		oTestObject.handleEvent("abrirChamadaEvent", disciplina1);
 		assertEquals(true, (oTestObject.state == ChamadaState.aberta));
