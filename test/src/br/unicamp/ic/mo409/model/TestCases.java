2b996a7010cd32799f124d6fe83aa9a7393c41c8
jevianna <jevianna@gmail.com>
2015-06-14 19:32:53 -0300

    Alteracao no diagrama de estados de Presenca
    Acrescentada a transicao de em_aula para em_aula, com a funcao
    receberTick.

--- test/src/br/unicamp/ic/mo409/model/TestCases.java
+++ test/src/br/unicamp/ic/mo409/model/TestCases.java
@@ -14,19 +14,19 @@ public class TestCases{
 	public void test1()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 165;
-		Integer raProfessor1 = -311;
-		Integer dataAula1 = 69;
-		Integer horaInicio1 = 240;
-		Integer idTurma3 = 584;
-		Integer raProfessor3 = -890;
-		Integer dataAula3 = 57;
-		Integer horaInicio3 = -238;
-		Integer idTurma5 = 153;
-		Integer raAluno5 = 319;
-		Integer dataAula5 = -493;
-		Integer horaInicio5 = 27;
-		Integer numTicks5 = -186;
+		Integer idTurma1 = 435;
+		Integer raProfessor1 = -5;
+		Integer dataAula1 = 35;
+		Integer horaInicio1 = 14;
+		Integer idTurma3 = 141;
+		Integer raProfessor3 = 651;
+		Integer dataAula3 = -69;
+		Integer horaInicio3 = -149;
+		Integer idTurma5 = -272;
+		Integer raAluno5 = -68;
+		Integer dataAula5 = 150;
+		Integer horaInicio5 = -577;
+		Integer numTicks5 = 252;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
 		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
@@ -41,19 +41,19 @@ public class TestCases{
 	public void test2()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 47;
-		Integer raProfessor1 = -308;
-		Integer dataAula1 = -332;
-		Integer horaInicio1 = 322;
-		Integer idTurma3 = -892;
-		Integer raProfessor3 = -65;
-		Integer dataAula3 = 244;
-		Integer horaInicio3 = 13;
-		Integer idTurma5 = -542;
-		Integer raAluno5 = 401;
-		Integer dataAula5 = -232;
-		Integer horaInicio5 = -467;
-		Integer numTicks5 = 87;
+		Integer idTurma1 = 0;
+		Integer raProfessor1 = -389;
+		Integer dataAula1 = -7;
+		Integer horaInicio1 = 153;
+		Integer idTurma3 = -611;
+		Integer raProfessor3 = 110;
+		Integer dataAula3 = 59;
+		Integer horaInicio3 = 31;
+		Integer idTurma5 = -28;
+		Integer raAluno5 = 251;
+		Integer dataAula5 = 122;
+		Integer horaInicio5 = 211;
+		Integer numTicks5 = -47;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
 		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
@@ -68,19 +68,19 @@ public class TestCases{
 	public void test3()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 70;
-		Integer raProfessor1 = -209;
-		Integer dataAula1 = -121;
-		Integer horaInicio1 = -253;
-		Integer idTurma3 = 4;
-		Integer raProfessor3 = 160;
-		Integer dataAula3 = 98;
-		Integer horaInicio3 = -35;
-		Integer idTurma5 = 470;
-		Integer raAluno5 = 351;
-		Integer dataAula5 = 248;
-		Integer horaInicio5 = 895;
-		Integer numTicks5 = 216;
+		Integer idTurma1 = 49;
+		Integer raProfessor1 = 60;
+		Integer dataAula1 = -21;
+		Integer horaInicio1 = 375;
+		Integer idTurma3 = -801;
+		Integer raProfessor3 = -70;
+		Integer dataAula3 = 507;
+		Integer horaInicio3 = 286;
+		Integer idTurma5 = 239;
+		Integer raAluno5 = 105;
+		Integer dataAula5 = 24;
+		Integer horaInicio5 = -353;
+		Integer numTicks5 = 258;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
 		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
@@ -95,20 +95,20 @@ public class TestCases{
 	public void test4()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = -40;
-		Integer raProfessor1 = 100;
-		Integer dataAula1 = -148;
-		Integer horaInicio1 = -871;
-		Integer idTurma3 = 105;
-		Integer raAluno3 = -423;
-		Integer dataAula3 = 82;
-		Integer horaInicio3 = -359;
-		Integer numTicks3 = 130;
+		Integer idTurma1 = -419;
+		Integer raProfessor1 = 40;
+		Integer dataAula1 = -73;
+		Integer horaInicio1 = -389;
+		Integer idTurma3 = -74;
+		Integer raAluno3 = 813;
+		Integer dataAula3 = -588;
+		Integer horaInicio3 = 761;
+		Integer numTicks3 = -530;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
 		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
+		oTestObject.handleEvent("receberTickEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
+		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
 		
 	}
 	
@@ -116,20 +116,20 @@ public class TestCases{
 	public void test5()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 190;
-		Integer raProfessor1 = 20;
-		Integer dataAula1 = -110;
-		Integer horaInicio1 = 134;
-		Integer idTurma3 = -411;
-		Integer raAluno3 = -25;
-		Integer dataAula3 = -380;
-		Integer horaInicio3 = 735;
-		Integer numTicks3 = 602;
+		Integer idTurma1 = -171;
+		Integer raProfessor1 = -141;
+		Integer dataAula1 = -34;
+		Integer horaInicio1 = 353;
+		Integer idTurma3 = -300;
+		Integer raAluno3 = -180;
+		Integer dataAula3 = 192;
+		Integer horaInicio3 = -106;
+		Integer numTicks3 = 250;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
 		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
+		oTestObject.handleEvent("receberTickEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
+		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
 		
 	}
 	
@@ -137,20 +137,20 @@ public class TestCases{
 	public void test6()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 660;
-		Integer raProfessor1 = -79;
-		Integer dataAula1 = 104;
-		Integer horaInicio1 = 201;
-		Integer idTurma3 = -440;
-		Integer raAluno3 = 283;
-		Integer dataAula3 = 238;
-		Integer horaInicio3 = 203;
-		Integer numTicks3 = -32;
+		Integer idTurma1 = -641;
+		Integer raProfessor1 = -555;
+		Integer dataAula1 = 211;
+		Integer horaInicio1 = 749;
+		Integer idTurma3 = 311;
+		Integer raAluno3 = 229;
+		Integer dataAula3 = 278;
+		Integer horaInicio3 = 559;
+		Integer numTicks3 = -97;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
 		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
+		oTestObject.handleEvent("receberTickEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
+		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
 		
 	}
 	
@@ -158,13 +158,19 @@ public class TestCases{
 	public void test7()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 442;
-		Integer raAluno1 = -642;
-		Integer dataAula1 = -52;
-		Integer horaInicio1 = -368;
-		Integer numTicks1 = 25;
+		Integer idTurma1 = -32;
+		Integer raProfessor1 = -241;
+		Integer dataAula1 = -73;
+		Integer horaInicio1 = 398;
+		Integer idTurma3 = 190;
+		Integer raAluno3 = -123;
+		Integer dataAula3 = 169;
+		Integer horaInicio3 = 324;
+		Integer numTicks3 = 14;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("calcularPresencaEvent", idTurma1, raAluno1, dataAula1, horaInicio1, numTicks1);
+		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
+		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
+		oTestObject.handleEvent("calcularPresencaEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
 		assertEquals(true, (oTestObject.state == PresencaState.presente));
 		
 	}
@@ -173,13 +179,19 @@ public class TestCases{
 	public void test8()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = -98;
-		Integer raAluno1 = 16;
-		Integer dataAula1 = 792;
-		Integer horaInicio1 = -392;
-		Integer numTicks1 = -118;
+		Integer idTurma1 = -431;
+		Integer raProfessor1 = -214;
+		Integer dataAula1 = 234;
+		Integer horaInicio1 = -199;
+		Integer idTurma3 = 751;
+		Integer raAluno3 = -135;
+		Integer dataAula3 = -283;
+		Integer horaInicio3 = 321;
+		Integer numTicks3 = 387;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("calcularPresencaEvent", idTurma1, raAluno1, dataAula1, horaInicio1, numTicks1);
+		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
+		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
+		oTestObject.handleEvent("calcularPresencaEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
 		assertEquals(true, (oTestObject.state == PresencaState.presente));
 		
 	}
@@ -188,11 +200,62 @@ public class TestCases{
 	public void test9()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer idTurma1 = 877;
-		Integer raAluno1 = -368;
-		Integer dataAula1 = -89;
-		Integer horaInicio1 = -79;
-		Integer numTicks1 = -762;
+		Integer idTurma1 = -374;
+		Integer raProfessor1 = 376;
+		Integer dataAula1 = 167;
+		Integer horaInicio1 = 1;
+		Integer idTurma3 = 33;
+		Integer raAluno3 = -15;
+		Integer dataAula3 = 935;
+		Integer horaInicio3 = -403;
+		Integer numTicks3 = -389;
+		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
+		oTestObject.handleEvent("checkInPresencaEvent", idTurma1, raProfessor1, dataAula1, horaInicio1);
+		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
+		oTestObject.handleEvent("calcularPresencaEvent", idTurma3, raAluno3, dataAula3, horaInicio3, numTicks3);
+		assertEquals(true, (oTestObject.state == PresencaState.presente));
+		
+	}
+	
+	@Test
+	public void test10()
+	{
+		Presenca oTestObject = new Presenca();
+		Integer idTurma1 = -58;
+		Integer raAluno1 = 0;
+		Integer dataAula1 = 347;
+		Integer horaInicio1 = 472;
+		Integer numTicks1 = 9;
+		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
+		oTestObject.handleEvent("calcularPresencaEvent", idTurma1, raAluno1, dataAula1, horaInicio1, numTicks1);
+		assertEquals(true, (oTestObject.state == PresencaState.presente));
+		
+	}
+	
+	@Test
+	public void test11()
+	{
+		Presenca oTestObject = new Presenca();
+		Integer idTurma1 = -553;
+		Integer raAluno1 = -326;
+		Integer dataAula1 = -527;
+		Integer horaInicio1 = -22;
+		Integer numTicks1 = 46;
+		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
+		oTestObject.handleEvent("calcularPresencaEvent", idTurma1, raAluno1, dataAula1, horaInicio1, numTicks1);
+		assertEquals(true, (oTestObject.state == PresencaState.presente));
+		
+	}
+	
+	@Test
+	public void test12()
+	{
+		Presenca oTestObject = new Presenca();
+		Integer idTurma1 = 169;
+		Integer raAluno1 = 452;
+		Integer dataAula1 = -184;
+		Integer horaInicio1 = -169;
+		Integer numTicks1 = 1;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("calcularPresencaEvent", idTurma1, raAluno1, dataAula1, horaInicio1, numTicks1);
 		assertEquals(true, (oTestObject.state == PresencaState.presente));
