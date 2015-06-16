4a4ed72234a31d52e4a116331d71b32376ff48d2
jevianna <jevianna@gmail.com>
2015-06-15 22:34:26 -0300

    Diagrama de estado de Presenca com guardas
    Coloquei guardas nas respectivas transicoes.
    Nao consegui colocar chamadaState = aberta, entao criei um atributo na
    classe Presenca para controlar isto (isChamadaAberta boolean).

--- test/src/br/unicamp/ic/mo409/model/TestCases.java
+++ test/src/br/unicamp/ic/mo409/model/TestCases.java
@@ -14,13 +14,9 @@ public class TestCases{
 	public void test1()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer numTicks5 = 495;
+		Integer numTicks1 = -25;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("checkOutPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.fora_de_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks5);
+		oTestObject.handleEvent("calcularPresencaEvent", numTicks1);
 		assertEquals(true, (oTestObject.state == PresencaState.presente));
 		
 	}
@@ -29,13 +25,9 @@ public class TestCases{
 	public void test2()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer numTicks5 = 331;
+		Integer numTicks1 = 285;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("checkOutPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.fora_de_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks5);
+		oTestObject.handleEvent("calcularPresencaEvent", numTicks1);
 		assertEquals(true, (oTestObject.state == PresencaState.presente));
 		
 	}
@@ -44,122 +36,7 @@ public class TestCases{
 	public void test3()
 	{
 		Presenca oTestObject = new Presenca();
-		Integer numTicks5 = 186;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("checkOutPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.fora_de_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks5);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
-		
-	}
-	
-	@Test
-	public void test4()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks3 = 79;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("receberTickEvent", numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		
-	}
-	
-	@Test
-	public void test5()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks3 = 37;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("receberTickEvent", numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		
-	}
-	
-	@Test
-	public void test6()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks3 = -41;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("receberTickEvent", numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		
-	}
-	
-	@Test
-	public void test7()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks3 = 546;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
-		
-	}
-	
-	@Test
-	public void test8()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks3 = 167;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
-		
-	}
-	
-	@Test
-	public void test9()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks3 = -724;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("checkInPresencaEvent");
-		assertEquals(true, (oTestObject.state == PresencaState.em_aula));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks3);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
-		
-	}
-	
-	@Test
-	public void test10()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks1 = -192;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks1);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
-		
-	}
-	
-	@Test
-	public void test11()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks1 = -97;
-		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
-		oTestObject.handleEvent("calcularPresencaEvent", numTicks1);
-		assertEquals(true, (oTestObject.state == PresencaState.presente));
-		
-	}
-	
-	@Test
-	public void test12()
-	{
-		Presenca oTestObject = new Presenca();
-		Integer numTicks1 = -700;
+		Integer numTicks1 = 230;
 		assertEquals(true, (oTestObject.state == PresencaState.em_branco));
 		oTestObject.handleEvent("calcularPresencaEvent", numTicks1);
 		assertEquals(true, (oTestObject.state == PresencaState.presente));
