93bcc0e301d1e198272e331a05eed94979c2bb2c
Luzizila <luzizila2009@gmail.com>
2015-06-23 12:23:06 -0300

    primeiros testes usando selenium em java e python

---
+++ H11 e H13 - Aluno - Login/teste.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class Teste(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:8100/#!/app/login"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_e(self):
+        driver = self.driver
+        driver.get(self.base_url + "/#!/app/login")
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys("35")
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("user")
+        driver.find_element_by_xpath("//button").click()
+	time.sleep( 10 )
+        driver.find_element_by_xpath("//ion-content/div/button").click()
+	time.sleep( 10 )
+        driver.find_element_by_xpath("//span/button").click()
+	time.sleep( 10 )
+        driver.find_element_by_link_text("Home").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
