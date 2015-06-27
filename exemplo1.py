c1f0dce1474ba2b144842424b305e252396ad94c
Luzizila <luzizila2009@gmail.com>
2015-06-26 22:59:50 -0300

    Testes de interfaces em execução

--- exemplo1.py
+++ exemplo1.py
@@ -7,22 +7,16 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
 
-class Teste(unittest.TestCase):
+class Exemplos1(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://localhost:8100/#/app/home"
+        self.base_url = "http://localhost:8100/#!/app/login"
         self.verificationErrors = []
         self.accept_next_alert = True
     
-    def test_e(self):
+    def test_exemplos1(self):
         driver = self.driver
-        driver.get(self.base_url + "/#/app/login")
-        time.sleep( 10 )
-        driver.find_element_by_xpath("//div[2]/ion-header-bar/div/span/button").click()
-        time.sleep( 10 )
-        driver.find_element_by_link_text("Aluno").click()
-        time.sleep( 10 )
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
